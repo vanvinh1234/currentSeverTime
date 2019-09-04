4d719a21c4637d5b74128fadfe22bbb084f9c036
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-09-04 11:41:31 +0700

    demo current sever time

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Current server time</title>
+</head>
+<body>
+<h1>Current Server time is: <?php echo date('Y-M-D h:m:s'); ?></h1>
+</body>
+</html>
